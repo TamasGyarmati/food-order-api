fa30f0dc801809fdc8a82f12c0dacbaf876d604e
Gyarmati Tamás <tamasgy56@gmail.com>
2025-11-24 21:24:21 +0100

    added migrations

---
+++ FoodOrder.Data/Migrations/20251124185340_OrderModelCreatorUserName2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrder.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderModelCreatorUserName2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorUserName",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorUserName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
